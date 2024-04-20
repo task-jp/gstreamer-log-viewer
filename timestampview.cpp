9e2689543dd0a8c50d7322b41585effa75c6feab
Tasuku Suzuki <stasuku@gmail.com>
2024-04-20 11:06:15 +0900

    Improve timestamp view to see gaps efficiently

--- timestampview.cpp
+++ timestampview.cpp
@@ -64,7 +64,7 @@ void TimestampView::paintEvent(QPaintEvent *event)
     const auto count = model->rowCount();
     if (count < 1) return;
 
-    const int w = width() / 5;
+    const int w = width() / 3;
     const int headerHeight = d->buddy->horizontalHeader()->height();
     const int h = height();
     const qreal dy = (qreal)(h - headerHeight) / (count - 1);
@@ -79,36 +79,35 @@ void TimestampView::paintEvent(QPaintEvent *event)
         return timestamp;
     };
 
-    if (d->cache.timeline.size() != QSize(w * 2, h) || d->cache.rowCount != count) {
+    const auto timestampMin = index2timestamp(0);
+    const auto timestampMax = index2timestamp(count - 1);
+    const qreal range = timestampMin.usecsTo(timestampMax);
 
-        const auto timestampMin = index2timestamp(0);
-        const auto timestampMax = index2timestamp(count - 1);
-        const qreal range = timestampMin.usecsTo(timestampMax);
-
-        d->cache.timeline = QImage(QSize(w * 2, h), QImage::Format_ARGB32_Premultiplied);
+    if (d->cache.timeline.size() != QSize(w, h) || d->cache.rowCount != count) {
+        d->cache.timeline = QImage(QSize(w, h), QImage::Format_ARGB32_Premultiplied);
         d->cache.timeline.fill(Qt::transparent);
         QPainter painter(&d->cache.timeline);
         painter.setRenderHint(QPainter::Antialiasing);
 
-        QPen pen(Qt::red);
-        pen.setWidth(3);
+        QPen pen(QColor(255, 0, 0, 10));
+        pen.setWidthF(0.5);
         painter.setPen(pen);
 
-        QPolygonF graph = { QPointF(0, headerHeight) };
-        for (int i = 1; i < count; i++) {
+        QPolygonF lines;
+        for (int i = 0; i < count; i++) {
             const auto timestamp = index2timestamp(i);
-            const qreal x = timestampMin.usecsTo(timestamp) / range * w * 2;
-            graph << QPointF(x, dy * i + headerHeight);
+            const qreal y = timestampMin.usecsTo(timestamp) / range * (h - headerHeight) + headerHeight;
+            lines << QPointF(0, y) << QPointF(w, y);
         }
-        painter.drawPolyline(graph);
+        painter.drawLines(lines);
     }
 
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
     painter.setOpacity(0.5);
     const auto brush = [&]() {
-        QLinearGradient gradient(w, h / 2, w * 5, h / 2);
-        gradient.setColorAt(0, Qt::red);
+        QLinearGradient gradient(w, h / 2, w * 3, h / 2);
+        gradient.setColorAt(0.0, Qt::red);
         gradient.setColorAt(0.5, Qt::green);
         gradient.setColorAt(1.0, Qt::blue);
         return QBrush(gradient);
@@ -130,25 +129,24 @@ void TimestampView::paintEvent(QPaintEvent *event)
     auto lastRow = lastIndex.row();
     if (lastRow < 0)
         lastRow = count - 1;
-
-    QRectF cursor(0, dy * firstRow + headerHeight, w * 2, dy * (lastRow - firstRow));
-    painter.drawRect(cursor);
-
-    QPolygonF polygon = { QPointF(w * 2, cursor.top()), QPointF(w * 3, headerHeight), QPointF(w * 3, h), QPointF(w * 2, cursor.bottom()) };
-    painter.drawPolygon(polygon);
-
-    QPointF origin(w, cursor.center().y());
     const auto firstTimestamp = index2timestamp(firstRow);
     const auto lastTimestamp = index2timestamp(lastRow);
-    const qreal range = qMax(firstTimestamp.usecsTo(lastTimestamp), 1);
+    const qreal range2 = qMax(firstTimestamp.usecsTo(lastTimestamp), 1);
+    const qreal yFirst = timestampMin.usecsTo(firstTimestamp) / range * (h - headerHeight) + headerHeight;
+    const qreal yLast = timestampMin.usecsTo(lastTimestamp) / range * (h - headerHeight) + headerHeight;
+
+    QPolygonF polygon = { QPointF(w, yFirst), QPointF(w * 2, headerHeight), QPointF(w * 2, h), QPointF(w, yLast) };
+    painter.drawPolygon(polygon);
+
+    QPointF origin(w, (yLast - yFirst) / 2);
     for (int row = firstRow; row <= lastRow; row++) {
         const auto rect = d->buddy->visualRect(firstIndex.siblingAtRow(row));
         const auto timestamp = index2timestamp(row);
-        const qreal y = firstTimestamp.usecsTo(timestamp) / range * (h - headerHeight);
+        const qreal y = firstTimestamp.usecsTo(timestamp) / range2 * (h - headerHeight);
         QPolygonF polygon = {
-            QPointF(w * 3, y + headerHeight),
-            QPointF(w * 5, rect.top() + 5 + headerHeight),
-            QPointF(w * 5, rect.bottom() - 5 + headerHeight),
+            QPointF(w * 2, y + headerHeight),
+            QPointF(w * 3, rect.top() + 5 + headerHeight),
+            QPointF(w * 3, rect.bottom() - 5 + headerHeight),
         };
         painter.drawPolygon(polygon);
     }
